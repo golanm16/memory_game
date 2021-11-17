4ca9064986f4be78178539e2de2bb00322b022ce
Golan <golanm16@gmail.com>
2021-11-17 08:37:40 +0200

    added function to refill the cards randomly, and added html injection

--- main.js
+++ main.js
@@ -1,61 +1,64 @@
-let cards = [
-    { name: 'A' },
-    { name: 'B' },
-    { name: 'B' },
-    { name: 'C' },
-    { name: 'A' },
-    { name: 'C' },
-    { name: 'J' },
-    { name: 'J' }
-];
-
-function get_rand_letter(alphabet) {
-
-    return alphabet[Math.floor(Math.random() * alphabet.length)];
+function delete_letter(str, i) {
+  return str.slice(0, i) + str.slice(i + 1, str.length);
 }
 
 function refill_cards() {
-    cards = [];
-    alphabet = "abcdefghijklmnopqrstuvwxyz".toUpperCase();
-    for (i = 0; i < 15; i++) {
-        letter = {name:get_rand_letter(alphabet)};
-        cards.push(letter);
-        cards.push(letter);
-    }
+  cards = [];
+  alphabet = "abcdefghijklmnopqrstuvwxyz".toUpperCase();
+  for (i = 0; i < 15; i++) {
+    letter_index = get_rand(0, alphabet.length);
+    cards.push({ name: alphabet[letter_index] });
+    cards.push({ name: alphabet[letter_index] });
+    alphabet = delete_letter(alphabet, letter_index);
+  }
+  return cards;
 }
 
 function get_rand(a, b) {
-    return a + Math.floor(Math.random() * b);
+  return a + Math.floor(Math.random() * b);
 }
 
 function swap(arr, i, j) {
-    [arr[i], arr[j]] = [arr[j], arr[i]];
+  [arr[i], arr[j]] = [arr[j], arr[i]];
 }
 
 function shuffle_cards(arr) {
-    for (i in arr) {
-        rand_index = get_rand(0, arr.length);
-        swap(arr, i, rand_index);
-    }
-    return arr;
+  for (i in arr) {
+    rand_index = get_rand(0, arr.length);
+    swap(arr, i, rand_index);
+  }
+  return arr;
 }
 
 function are_equal(arr, i, j) {
-
-    return arr[i].name == arr[j].name;
+  return i != j && arr[i].name === arr[j].name;
 }
 
-function remove_cards(i, j) {
-    cards.splice(i, 1);
-    cards.splice(j, 1);
+function remove_cards(cards, i, j) {
+  cards.splice(i, 1);
+  cards.splice(j, 1);
+}
+
+function create_card_element(document, card) {
+  const board = document.getElementById("board");
+  const card_element = document.createElement("div");
+  card_element.innerHTML = card.name;
+  board.appendChild(card_element);
 }
 
 function main() {
-    refill_cards()
-    console.log(JSON.stringify(cards));
-    shuffle_cards(cards);
-    console.log(cards);
-
+  cards = refill_cards();
+  console.log(JSON.stringify(cards));
+  shuffle_cards(cards);
+  console.log(cards);
+  // const board = document.getElementById("board");
+  // const card_element = document.createElement("div");
+  // card_element.innerHTML = cards[0];
+  // console.log(board);
+  // board.appendChild(card_element);
+  for (card of cards) {
+    create_card_element(document, card);
+  }
 }
 
 main();
